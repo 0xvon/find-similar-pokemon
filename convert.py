3944635b39df666160788b6f77461d098e23522f
Masato TSUTSUMI <masato.11.soccer@gmail.com>
2019-01-09 20:40:53 +0900

    start clustering

--- convert.py
+++ convert.py
@@ -1,27 +1,16 @@
-import pandas as pd
-import mahotas as mh
 import numpy as np
 from PIL import Image
-import cv2
 from glob import glob
-from skimage import data
-from sklearn.preprocessing import StandardScaler
-from sklearn.decomposition import PCA
-from sklearn.cluster import KMeans
 
 #画像の取得
 imgs = glob("./material_images/*.png") + glob("./material_images/*.jpg")
 
-#画像の処理
+#画像の処理・配列化
 datasets = []
 for i in imgs:
     img = Image.open(i)
     img = img.convert('L')
     img = img.resize((100, 100))
     img = np.array(img)
-    img = img.flatten()
+    img = img.ravel()
     datasets.append(img)
-
-    # datasets.append(img)
-
-print(datasets)
--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 import networkx as nx
 import community
 import matplotlib.pyplot as plt
+from convert import datasets
+import matplotlib
+from sklearn.cluster import KMeans
+
+model = KMeans(n_clusters=100)
+model.fit(datasets)
+
+labels = model.labels_
+
+print(labels)
