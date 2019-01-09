e0c8c8a58244673e3416e5923e978d0b06b3af50
Masato TSUTSUMI <masato.11.soccer@gmail.com>
2019-01-09 19:04:59 +0900

    prepare datasets

--- convert.py
+++ convert.py
@@ -1,21 +1,27 @@
 import pandas as pd
 import mahotas as mh
 import numpy as np
+from PIL import Image
+import cv2
 from glob import glob
+from skimage import data
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans
 
 #画像の取得
 imgs = glob("./material_images/*.png") + glob("./material_images/*.jpg")
-alldescriptors = []
 
-#グレイスケール
-for im in imgs:
-    im = mh.imread(im, as_gray=True)
-    im = im.astype(np.uint8)
-    alldescriptors.append(surf.surf(im, descriptor_only=True))
+#画像の処理
+datasets = []
+for i in imgs:
+    img = Image.open(i)
+    img = img.convert('L')
+    img = img.resize((100, 100))
+    img = np.array(img)
+    img = img.flatten()
+    datasets.append(img)
 
-# df = pd.read_csv("./pokemon.csv", header=None)
-# X, y = df.iloc[1:, 1:].values, df.iloc[1:, 0].values
-# sc = StandardScaler()
+    # datasets.append(img)
+
+print(datasets)
